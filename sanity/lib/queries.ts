bf1722b29bb89368e8f1daf495ec589cc487baa5
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-09-15 15:49:03 +0300

    Add `Skeleton` and `View` components; upgrade dependencies: (#1)
    - Introduced `Skeleton` component with pulse animation.
    - Added `View` component to display startup views using Sanity query.
    - Upgraded packages, including `next` to `15.6.0-canary.7` and dependencies in `@inquirer` suite.
    - Updated `.editorconfig` to enforce `max_line_length` of 80.

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -3,12 +3,18 @@ import type {Author, Startup} from "@/sanity/types";
 
 export type StartupQueryData = StartupPost[];
 
-export type StartupPost = Omit<Startup, "author"> & {
+export type StartupPost = Omit<Startup, "author" | "pitch"> & {
   author?: Pick<Author, "_id" | "name" | "image" | "bio">;
 };
 
-export const STARTUP_QUERY = defineQuery(`
-*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+export type StartupData = Omit<Startup, "author"> & {
+  author?: Pick<Author, "_id" | "name" | "username" | "image" | "bio">;
+};
+
+export type StartupViews = Pick<Startup, "_id" | "views">;
+
+export const STARTUP_QUERY =
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
   _id,
   title,
   slug,
@@ -23,5 +29,30 @@ export const STARTUP_QUERY = defineQuery(`
   description,
   category,
   image
-}
-`);
+}`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id,
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id,
+    name,
+    username,
+    image,
+    bio
+  },
+  views,
+  description,
+  category,
+  image,
+  pitch
+}`);
+
+export const STARTUP_VIEWS_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id,
+  views
+}`);
