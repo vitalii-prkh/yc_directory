c50fc8ba3bea77ccaca6af2f268c8d9fe03a98b4
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-09-15 16:26:58 +0300

    Add `writeClient` for Sanity write operations; patch `View` component to update views:
    - Introduced `writeClient` to handle Sanity write operations with token validation.
    - Updated `View` component to increment and save view count asynchronously using `writeClient`.
    - Adjusted `useCdn` configuration in `client` setup for read and write purposes.
    - Added `easymde` and related dependencies to `package.json`.

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,20 @@
+import {after} from "next/server";
 import Ping from "@/components/Ping";
 import {client} from "@/sanity/lib/client";
 import {STARTUP_VIEWS_QUERY, type StartupViews} from "@/sanity/lib/queries";
+import {writeClient} from "@/sanity/lib/write-client";
 
 async function View(props: {id: string}) {
   const data = await client
-    .withConfig({useCdn: true})
+    .withConfig({useCdn: false})
     .fetch<StartupViews>(STARTUP_VIEWS_QUERY, {id: props.id});
+
+  after(async () => {
+    await writeClient
+      .patch(props.id)
+      .set({views: (data.views || 0) + 1})
+      .commit();
+  });
 
   return (
     <div className="view-container">
